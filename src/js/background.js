e8b9bf313a30f37e05a1cfc37affeb899dcaec1c
rg3 <1xab@protonmail.com>
2019-11-11 05:28:16 +0100

    Refactor background.js

--- src/js/background.js
+++ src/js/background.js
@@ -5,23 +5,20 @@ const MAX_BITMAPS_SIZE = 6;
 const bitmaps = [];
 
 chrome.commands.onCommand.addListener((command) => {
-  switch(command) {
-    case "take_screenshot": {
-      chrome.tabCapture.capture({audio: false, video: true}, function(stream) {
-        const track = stream.getVideoTracks()[0];
-        const frame = new ImageCapture(track).grabFrame();
-        frame.then((bitmap) => {
-          if(bitmaps.length === MAX_BITMAPS_SIZE) {
-            bitmaps.pop();
-          }
-          bitmaps.unshift(new ImageBitmap(bitmap));
-          track.stop();
-          notify("You took a screenshot");
-          window.SCREENSHOT_COUNT += 1;
-        }).catch(track.stop);
-      });
-    }
-    break;
+  if(command === "take_screenshot") {
+    chrome.tabCapture.capture({audio: false, video: true}, (stream) => {
+      const track = stream.getVideoTracks()[0];
+      const grabFrame = new ImageCapture(track).grabFrame();
+      grabFrame.then((bitmap) => {
+        if(bitmaps.length === MAX_BITMAPS_SIZE) {
+          bitmaps.pop();
+        }
+        bitmaps.unshift(new ImageBitmap(bitmap));
+        track.stop();
+        notify("You took a screenshot");
+        window.SCREENSHOT_COUNT += 1;
+      }).catch(track.stop);
+    });
   }
 });
 
@@ -40,5 +37,4 @@ chrome.runtime.onMessage.addListener((req) => {
  * getBackgroundPage((page) => page.SCREENSHOT_COUNT)
 */
 window.SCREENSHOT_COUNT = 0;
-window.MAX_BITMAPS_SIZE = MAX_BITMAPS_SIZE;
 window.bitmaps = bitmaps;
