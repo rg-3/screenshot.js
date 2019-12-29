4d9ad7efc0da859b7e39b2831052a80195e57f27
rg <1xab@protonmail.com>
2019-12-29 11:54:55 +0100

    Improve code comment in app.js

--- src/js/lib/background/app.js
+++ src/js/lib/background/app.js
@@ -96,15 +96,15 @@ export default function() {
       return codeCache[this.videoSize];
     } else if(!captureHTML5VideoTemplate) {
       /*
-         This should not happen, it means the fetch() call made in
-         setCaptureHTML5VideoTemplate function has failed or has not completed
-         yet. Since setCaptureHTML5VideoTemplate is making a request for a file
-         that is served from disk and belongs to the extension, its failure
-         would indicate something unusual has happened.
+         This should not happen under normal conditions, it means the fetch()
+         call made in setCaptureHTML5VideoTemplate function has failed or has
+         not completed yet. Since setCaptureHTML5VideoTemplate is making a
+         request for a file that is served from disk and belongs to the
+         extension, its failure would indicate something unusual has happened.
 
-         The handling of this error is.... weird. We literally show
-         'captureHTML5VideoTemplate_Not_Initialized' as a notification.
-         (See src/js/background.js)
+         The handling of this error could be improved. For the time being we show
+         'captureHTML5VideoTemplate_Not_Initialized' as a notification in
+         'src/js/background.js' (where this function is called from).
       */
       return 'captureHTML5VideoTemplate_Not_Initialized';
     } else {
