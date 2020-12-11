bbc4ee6b8c59744a7611b1050ec77ed50ad03977
rg <1aab@protonmail.com>
2020-12-11 06:55:08 +0100

    replace emojis with help icon

--- src/js/browser-action/tippy-help.js
+++ src/js/browser-action/tippy-help.js
@@ -1,7 +1,5 @@
 export default function() {
-  const el  = document.getElementById('help-icon');
-  const img = el.querySelector('img');
-  tippy(el, {
+  tippy(document.getElementById('help-icon'), {
     theme: 'light-border',
     trigger: 'click',
     arrow: true,
@@ -9,8 +7,6 @@ export default function() {
     maxWidth: 420,
     offset: [0, 5],
     arrow: false,
-    onShow: () => img.src = '/images/icons/help-open-icon.svg',
-    onHide: () => img.src = '/images/icons/help-icon.svg',
     content: () => {
       const el = document.getElementById('popover-help-content').cloneNode(true);
       el.classList.remove('hidden');
